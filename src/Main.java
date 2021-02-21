16c24cd3937c945abdc27eb2d656365171976a0f
detailnur <detailnur@gmail.com>
2021-02-21 17:07:12 +0300

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int count = 1000;
+        int cashback = 3;
+        int percent = 100;
+        System.out.println(count * cashback / percent);
+    }
+}
